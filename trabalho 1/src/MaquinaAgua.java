62f3366b7586d92df69f78a52184522c07441086
Bruno Silva da Rosa <33634327+brunosilva109@users.noreply.github.com>
2024-03-08 20:51:27 -0300

    Update MaquinaAgua.java
    1.1 correto

--- trabalho 1/src/MaquinaAgua.java
+++ trabalho 1/src/MaquinaAgua.java
@@ -1,43 +1,50 @@
-package src;
-
 class MaquinaAgua {
-    private int bombonaDagua = 20000; // 20 litros
-    private int copos200 = 100;
-    private int copos300 = 100;
+    private int agua = 0; // 20 litros
+    private int copos200 = 0;
+    private int copos300 = 0;
 
     void servirCopo200() {
-        if (bombonaDagua < 200 && copos200 <= 0) {
+        if (agua < 200 && copos200 <= 0) {
             System.out.println("Sem Copos e agua");
-        } else if (bombonaDagua < 200) {
+        } else if (agua < 200) {
             System.out.println("Sem agua");
         } else if (copos200 <= 0) {
             System.out.println("Sem Copos");
         } else {
-            bombonaDagua = bombonaDagua - 200;
+            agua = agua - 200;
             copos200--;
         }
     }
 
     void servirCopo300() {
-        if (bombonaDagua < 300 && copos200 <= 0) {
+        if (agua < 300 && copos300 <= 0) {
             System.out.println("Sem Copos e agua");
-        } else if (bombonaDagua < 300) {
+        } else if (agua < 300) {
             System.out.println("Sem agua");
         } else if (copos300 <= 0) {
             System.out.println("Sem Copos");
         } else {
-            bombonaDagua = bombonaDagua - 300;
+            agua = agua - 300;
             copos300--;
         }
     }
 
     void abastecerAgua() {
-        bombonaDagua = 20000;
+        agua = 20000;
     }
     void abastecerCopo200(){
         copos200 = 100;
     }
     void abastecerCopo300(){
         copos300 = 100;
+    }
+    int agua(){
+        return agua;
+    }
+    int copos200(){
+        return copos200;
+    }
+    int copos300(){
+        return copos300;
     }
 }
